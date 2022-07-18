6729a13c8d6fe98485c2c3dbfcd1315cd972db0e
wamballa <steve.tyrell@gmail.com>
2022-07-18 15:55:06 +0100

    added models

--- Assets/CreditCreatori.cs
+++ Assets/CreditCreatori.cs
@@ -1,10 +1,25 @@
 /*
-
+Credit the creator
 
 Mario by VERST is licensed under Creative Commons Attribution
 
+Chain Chomp (Super Mario Bros) by Yanez Designs is licensed under Creative Commons Attribution
+
+Flagpole (Super Mario Bros) by Yanez Designs is licensed under Creative Commons Attribution
 
 
+Mario Shell Red by Thomas Fugier is licensed under Creative Commons Attribution
 
+Super Mario World - Boo by Lucartoon is licensed under Creative Commons Attribution
+
+Mushroom Mario by Federico.Salvatierra is licensed under Creative Commons Attribution
+
+Mario Cap by WalidEH is licensed under Creative Commons Attribution
+
+Mario Coin by Twisty_z is licensed under Creative Commons Attribution
+
+Brick Block (Super Mario Bros.) by aidanp is licensed under Creative Commons Attribution
+
+Princess Peach - Luigis Mansion 3 by Logan is licensed under Creative Commons Attribution
 
 */
